144cd077420803089d9c5f8410583af1e155f827
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-08 02:10:58 +0300

    tuchanges

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,12 +1,20 @@
 -- Creating a stored procedure
 -- Adds a new correction to the bonus
 DELIMITER $$ ;
-CREATE PROCEDURE add_bonus(IN user_id int, IN project_name VARCHAR(255), IN score int)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-		INSERT INTO projects (name) VALUES (project_name);
-	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+  DECLARE project_id INT;
+  
+  -- Check if the project already exists in the table
+  SELECT id INTO project_id FROM projects WHERE name = project_name;
+  
+  -- If the project doesn't exist, insert it and get its id
+  IF project_id IS NULL THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    SET project_id = LAST_INSERT_ID();
+  END IF;
+  
+  -- Insert the correction
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END;$$
 DELIMITER ;
