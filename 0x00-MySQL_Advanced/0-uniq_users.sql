9d079c9105eceaa0ee5d7b001f82d40a4ae71576
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-07 22:53:55 +0300

    latest

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creating a table 
 -- The table should define some unique characters
-CREATE IF NOT EXISTS TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255)
