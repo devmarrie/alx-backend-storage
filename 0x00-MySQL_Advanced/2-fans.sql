af231190b6afdb35d12c92f7a60ebab88636b593
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-08 00:03:14 +0300

    separate

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- Select origin and their total number of funs
 -- Rank the output in DESC order
-SELECT origin,
-   SUM(fans) AS nb_fans,
+SELECT origin, 
+  SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
