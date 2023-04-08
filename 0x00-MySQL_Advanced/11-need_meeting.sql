543e3a828ce4b273f415e87f82af2edded1e6f00
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-08 04:19:32 +0300

    create a view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- creates  a view
+-- lists all students with score under 80
+CREATE VIEW need_meeting
+AS SELECT *
+FROM students
+WHERE score < 80
+AND (last_meeting = NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
