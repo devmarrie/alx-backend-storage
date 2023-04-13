d1d4945edf040023f7477b0a35c50cc2fbae6ce0
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-14 02:57:10 +0300

    check if none not perform truth test on it

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""lists all documents in a collection
+"""
+import pymongo
+
+def list_all(mongo_collection):
+    """mongo_collection will be the pymongo collection object
+       Return an empty list if no document in the collection
+    """
+    if mongo_collection is None:
+        return []
+    return [doc for doc in mongo_collection.find()]
+
+    
